afff9dd8627e36b67b662fd0bc71cc30ad2ed162
smb-lemon <smblemon9@gmail.com>
2024-09-23 22:02:09 +0600

    Initial commit

---
+++ basic-react/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import ProductList from "./ProductList.jsx";
+
+function App() {
+    return (
+   <>
+        <ProductList />
+   </>
+    );
+}
+
+export default App;
---
+++ basic-react/src/ProductList.jsx
@@ -0,0 +1,12 @@
+import Product from "./Product.jsx";
+
+function ProductList() {
+    return (
+        <>
+        <Product />
+        <Product />
+        <Product />
+    </>
+    );
+}
+export default ProductList;
